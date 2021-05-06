c18508eafed40c120e52e0025a93f35675c1af27
Raelinda Shoates <redfire809@gmail.com>
2021-05-05 22:08:27 -0400

    created a function that generates colors at random

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ div.style.height = '29px';
 div.style.border = 'thin solid black';
 div.style.display = 'inline-block';
 
+function rdmColor(element) {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  element.style.backgroundColor = "#" + randomColor;
+}
+
 function row() {
   for(let i = 0; i < 16; i++) {
       let clone = div.cloneNode(true);
