3cbe297904453488fe8f595c1d80bdf78b977f48
Raelinda Shoates <redfire809@gmail.com>
2021-05-05 21:55:30 -0400

    created a function that adds a row of cells to grid

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const container = document.querySelector('#gridContainer');
+const div = document.createElement('div');
+div.style.width = '29px';
+div.style.height = '29px';
+div.style.border = 'thin solid black';
+div.style.display = 'inline-block';
+
+function row() {
+  for(let i = 0; i < 16; i++) {
+      let clone = div.cloneNode(true);
+      clone.classList.add('cell');
+     
+     container.appendChild(clone);
+  }
+}
+
 
 
 
