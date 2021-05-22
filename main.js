6c5ec19f2d3afdaa33b872947ce2c72e1fe1d52e
Raelinda Shoates <redfire809@gmail.com>
2021-05-22 17:45:30 -0400

    renamed row and column function for a better description. Added more elements

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
 const container = document.querySelector('#gridContainer');
 const div = document.createElement('div');
-div.style.width = '29px';
-div.style.height = '29px';
 div.style.border = 'thin solid black';
 div.style.display = 'inline-block';
+let gridSizeBtn = document.getElementById('gridSize');
+let colorInputLabel = document.getElementById('label');
+let colorInput = document.getElementById('color');
+let color;
+let rainbowBtn = document.getElementById('rainbow')
+let eraseBtn = document.getElementById('erase');
+let clearBtn = document.getElementById('clear')
+let userAnswer;
 
 function rdmColor(element) {
   const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  element.style.backgroundColor = "#" + randomColor;
+  element.style.backgroundColor = "#" + randomColor; 
+  return true;
 }
 
-function row() {
+function mediumGridCellGenerator() {
   for(let i = 0; i < 16; i++) {
-      let clone = div.cloneNode(true);
+    div.style.width = '29px';
+    div.style.height = '29px';
+    let clone = div.cloneNode(true);
       clone.classList.add('cell');
       clone.addEventListener('mouseenter', () => {
-        clone.style.backgroundColor = rdmColor(clone);
-      })
-     container.appendChild(clone);
+       btnClicked(clone);
+    })
+    clearBtn.addEventListener('click', () => {
+      clone.style.backgroundColor = null;
+    })
+    gridSizeBtn.addEventListener('click', () => {
+        if(userAnswer === 'large' || userAnswer === 'small') {
+          container.removeChild(clone);  
+        }
+      
+    }) 
+   container.appendChild(clone);
+  
   }
 }
 
-function column() {
+function mediumGridTotalCells() {
   for (let i = 0; i < 32; i++) {
-      row();
+      mediumGridCellGenerator();
       i++;
   }
 }
-
-column();
+mediumGridTotalCells();
 
 
 
